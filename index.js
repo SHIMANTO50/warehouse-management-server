c9b36a54e3704d9d98b6717156a171b747ae60cd
SHIMANTO50 <shimantotsp@gmail.com>
2022-05-03 01:35:55 +0600

    items loaded

--- index.js
+++ index.js
@@ -11,12 +11,25 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ulez6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-    const collection = client.db("test").collection("devices");
-    console.log("Connection is OK");
-    // perform actions on the collection object
-    client.close();
-});
+
+async function run() {
+    try {
+        await client.connect();
+        const itemsCollection = client.db('warehouse').collection('items');
+
+        app.get('/item', async (req, res) => {
+            const query = {};
+            const cursor = itemsCollection.find(query);
+            const item = await cursor.toArray();
+            res.send(item);
+        })
+    }
+    finally {
+
+    }
+
+}
+run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
